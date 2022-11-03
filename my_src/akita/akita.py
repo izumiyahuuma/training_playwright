0b0a7aa9e677e52b451425061f3f0ed00e239f29
sato.taku <59036645+izumiyahuuma@users.noreply.github.com>
2022-11-03 11:28:10 +0900

    各入札執行課所毎に検索できるところまで

--- my_src/akita/akita.py
+++ my_src/akita/akita.py
@@ -4,7 +4,10 @@ from base import *
 with sync_playwright() as p:
     browser = BaseBrowser(browser=p.chromium.launch(headless=False))
     page = browser.new_page(options={})
-    page.navigate('http://cals05.pref.akita.lg.jp/')
-    page.click('//html/body/div[2]/ul/li[2]/a')
-    page.click_to_navigate('//html/body/div[3]/div[2]/table/tbody/tr/td[1]/table/tbody/tr[1]/td/table/tbody/tr[3]/td[2]/table/tbody/tr[1]/td[2]/a')
-    
+    page.navigate('http://cals05.pref.akita.lg.jp/ecydeen/do/PPI/koukoku')
+    options: list[str] = page.fetch_option_values(
+        '//html/body/div/form/table[3]/tbody/tr[2]/td/table/tbody/tr[8]/td[2]/select/option')
+
+    for option in options:
+        page.select('//html/body/div/form/table[3]/tbody/tr[2]/td/table/tbody/tr[8]/td[2]/select', option)
+        page.click_to_navigate('//html/body/div/form/table[3]/tbody/tr[2]/td/a/img')
--- my_src/akita/base.py
+++ my_src/akita/base.py
@@ -1,4 +1,4 @@
-from playwright.sync_api import Browser, Page
+from playwright.sync_api import Browser, Page, Locator
 
 
 class BasePage:
@@ -22,6 +22,15 @@ class BasePage:
         self.click(selector)
         self.__page.wait_for_load_state()
         self.__page.wait_for_timeout(self.__options.get('timeout', 1000))
+
+    def fetch_option_values(self, selector: str) -> list[str]:
+        options: Locator = self.__page.locator(selector)
+        values: list[str] = [options.nth(i).get_attribute('value') for i in range(options.count())]
+        values.remove('')
+        return values
+
+    def select(self, select_box_selector: str, option_value: str):
+        self.__page.locator(select_box_selector).select_option(value=option_value)
 
     def click(self, selector: str):
         self.__page.click(selector)
